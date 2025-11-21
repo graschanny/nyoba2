d4ef7bfb0308375dc4742c8ada9ad67720c675af
hanny <grasc.hanny@gmail.com>
2025-11-21 09:58:56 +0700

    menambahkan style

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Hello, Coxo!";
-?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Coxo</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+    <?php
+    echo "Hello, Coxo!";
+    ?>
+</body>
+</html>
